bd99a5b92738b5399d79d548457b66041f73eea8
bookstack@0c1241d1-2811-0410-b6f5-f555c02b9b86 <none@none>
2007-03-04 22:59:26 +0000

    Add Trove Classifiers.
    --HG--
    extra : convert_revision : svn%3A0c1241d1-2811-0410-b6f5-f555c02b9b86/trunk%4050

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
 from distutils.core import setup
+from sys import version
+if version < '2.2.3':
+	from distutils.dist import DistributionMetadata
+	DistributionMetadata.classifiers = None
+	DistributionMetadata.download_url = None
+
+
 setup(name='pyaws',
-	version='0.1',
+	version='0.1.0',
 	package_dir={'pyaws': ''},
 	packages=['pyaws'],
+	author='Kun Xi',
+	author_email='quinnxi@gmail.com', 
+	description='A Python wrapper for Amazon Web Service',
+	download_url='http://downloads.sourceforge.net/pyaws/pyaws-0.1.0.tar.gz?use_mirror=osdn',
+	classifiers=[
+		'Development Status :: 2 - Pre-Alpha',
+		'Environment :: Web Environment',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: Python Software Foundation License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Topic :: WWW/HTTP',
+          ]
 )
