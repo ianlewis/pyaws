2a81e14e85e312918da0dbbf6a7a51f63f738209
bookstack@0c1241d1-2811-0410-b6f5-f555c02b9b86 <none@none>
2007-04-09 00:16:57 +0000

    PyAWS-0.2.0 released!
    --HG--
    extra : convert_revision : svn%3A0c1241d1-2811-0410-b6f5-f555c02b9b86/trunk%4063

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ if version < '2.2.3':
 
 
 setup(name='pyaws',
-	version='0.1.0',
+	version='0.2.0',
 	package_dir={'pyaws': ''},
 	packages=['pyaws'],
 	author='Kun Xi',
-	author_email='quinnxi@gmail.com', 
+	author_email='kunxi@kunxi.org', 
 	description='A Python wrapper for Amazon Web Service',
 	long_description='PyAWS is a Python wrapper for the latest Amazon Web Service. It is designed to pave the way for Python developers to interactivate AWS. This project is forked from the code base of pyamazon. The Amazone E-Commerce Services is supported.', 
 	url='http://pyaws.sourceforge.net',
